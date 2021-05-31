4821e1f5dbb9a14af1ed56c5d2b6a86b70fa2936
Yellow_Ledbetter <>
2021-05-31 17:33:41 +0000

    defined custom note type

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
 import React from 'react';
+import { Footer } from "./components/Footer/Footer";
+import { Logo } from "./components/Logo/Logo";
+import styles from "./App.module.css";
 
 export const App = () => {
-  return null;
+  return (
+    <div className={styles.app}>
+      <Logo />
+      <main className={styles.content} />
+      <Footer />
+    </div>
+  );
 }
